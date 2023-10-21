83af31da518e2b155235bf615f406250c11ffd50
nicolasmartinb <nicolasmartinbidart@gmail.com>
2023-10-21 21:56:22 +0200

    segunda parte proyecto

--- index.js
+++ index.js
@@ -68,8 +68,119 @@ const resta = (a,b) => a - b;
 
 
 const iva = (precio) => precio * 0.21;
-let precioProducto = 60000;
-let descuento = 1000;
+let precioProducto = parseInt(prompt("Ingrese Precio "));
+let descuento = parseInt(prompt("Ingrese Descuento Deseado"));
+
 
 let precioFinal = resta(suma(precioProducto, iva(precioProducto)), descuento);
-console.log(precioFinal);
+
+alert("El precio final seria de " + precioFinal);
+
+const producto = {
+  nombre: "Campe Chelsea", 
+  precio: 45000,
+  stock: 2,
+};
+
+console.log(producto.nombre);
+console.log(producto.precio);
+console.log(producto.stock);
+
+let usuario = prompt("Ingrese su codigo de descuento");
+alert("Perfecto! Su precio final es de " +producto.precio);
+
+function Producto(info){
+  this.codigo = info.codigo;
+  this.precio = info.precio;
+  this.stock = info.stock;
+}
+
+const producto1 = new Producto ({
+codigo: "Campe-Chelsea-ING" ,
+precio: 250000,
+stock: 2,
+});
+console.log(producto1)
+
+const producto3 = new Producto ({
+  codigo: "Campe-España-ES" ,
+  precio: 20000,
+  stock: 2,
+  });
+  console.log(producto3)
+
+
+  function Producto(codigo, precio, stock){
+    this.codigo = codigo;
+    this.precio = precio;
+    this.stock = stock;
+    this.hablar = function (){
+      console.log(`Hola! soy ${this.codigo}`);
+    };
+  }
+
+  const producto2= new Producto ("Campe España", 35000, 2);
+  producto2.hablar();
+
+
+  //arrays
+  const listaProductos = [];
+  let cantidad = 6
+
+  do{
+    let entrada = prompt ("Ingresar nombre de la prenda que desea");
+    listaProductos.push(entrada.toUpperCase());
+    console.log(listaProductos.length);
+  }while(listaProductos.length != cantidad)
+
+  const nuevaLista = listaProductos.concat (["Campera Adidas","Campe Seleccion Italia"]);
+  alert (nuevaLista.join ("\n"));
+
+  const camperas = [{id: 1, seleccion : "Española", precio : "35000"},
+ {id: 2, seleccion : "Italiana" , precio : "25000"},
+ {id: 3, seleccion : "Francesa", precio : "5000"}];
+
+ for (const campera of camperas) {
+  console.log(campera.id);
+  console.log (campera.seleccion);
+  console.log (campera.precio);
+ }
+
+ const buscado = camperas.find (campera => campera.id ===3)
+ console.log(buscado)
+
+ const existe = camperas.find(campera => campera.seleccion ==="Argentina")
+ console.log (existe)
+
+ const rebajas = camperas.find(campera => campera.precio <3000)
+ console.log (rebajas)
+
+ const listaCamperas = camperas.map(campera => campera.seleccion)
+ console.log(listaCamperas);
+
+ const resultado = camperas.filter((el) => el.seleccion.includes('Italiana'))
+ const resultado2 = camperas.filter((el)=> el.precio <15000)
+
+ console.log(resultado)
+ 
+
+
+ class prenda{
+  constructor (nombre, precio){
+    this.nombre = nombre.toUpperCase();
+    this.precio = parseFloat(precio);
+    this.vendido = false;
+  }
+  sumaIva(){
+    this.precio = this.precio * 1.21;
+  }
+ }
+
+ const prendas = [];
+ prendas.push(new prenda ("Campera Manchester", "25000"));
+ prendas.push(new prenda ("Chomba adidas 2005","35500"));
+ prendas.push(new prenda ("Conjunto Chelsea 2006","65500"));
+ prendas.push(new prenda ("Conjunto Seleccion Española 2010","60000"));
+
+ for (const prenda of prendas)
+ prenda.sumaIva();
