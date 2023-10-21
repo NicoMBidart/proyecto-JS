6b404da7547a50306fe26435981a7dc5a159f9e4
nicolasmartinb <nicolasmartinbidart@gmail.com>
2023-10-21 18:09:06 +0200

    Segunda Pre entrega

--- index.js
+++ index.js
@@ -28,4 +28,48 @@ let ingreseNumero = parseInt (prompt("Ingrese un número"));
  } else {
    alert("La cantidad de cuotas ingresada no es válida. Debe ser mayor que 0.");
  }
- 
+ //segunda pre Entrega//
+
+ function aviso(){
+  console.log("Esta es mi segunda PreEntrega");
+}
+
+aviso();
+
+function pedirDato(direccion, saludo){
+  console.log(`${saludo} ${direccion}`);
+}
+
+pedirDato ("Calle siempre viva 123","Su pedido será enviado a : ");
+pedirDato ("Calle Falsa 2345","Perfecto! tu pedido se va a enviar a: ");
+
+function buscar(prenda){
+  return `Tenemos tu prenda ${prenda} lista`;
+  
+}
+
+function empaquetado(paqueteria){
+  return `${paqueteria} para ser enviada a la dirección que nos indicaste `;
+
+}
+
+function envioDePaquete(destino){
+  console.log(`${destino} Y listo para salir , que lo disfrutes!`);
+
+}
+
+let armadoPaquete = buscar ("Campe Chelsea");
+let empaquetar = empaquetado (armadoPaquete);
+envioDePaquete(empaquetar);
+
+//funciones para calcular iva//
+const suma = (a , b) => a + b;
+const resta = (a,b) => a - b;
+
+
+const iva = (precio) => precio * 0.21;
+let precioProducto = 60000;
+let descuento = 1000;
+
+let precioFinal = resta(suma(precioProducto, iva(precioProducto)), descuento);
+console.log(precioFinal);
